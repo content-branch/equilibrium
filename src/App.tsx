b72c47b0b0af3a8384f7a1349a0ccdcdc1bd23ab
Andy Randrianirina <andy.randrianirina@gmail.com>
2021-06-28 19:09:07 +0400

    External folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,13 +6,42 @@ import Views from './views';
 import { Route, Switch } from 'react-router-dom';
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import { THEME_CONFIG } from './configs/AppConfig';
+import { track, dispatch, init as initAnalytics } from "./util/analytics";
+import * as reactHotkeys from "react-hotkeys";
+
+const context = {
+  source: "amplication-client",
+};
 
 const themes = {
   dark: `${process.env.PUBLIC_URL}/css/dark-theme.css`,
   light: `${process.env.PUBLIC_URL}/css/light-theme.css`,
 };
 
+export const enhance = track<keyof typeof context>(
+  // app-level tracking data
+  context,
+
+  {
+    dispatch,
+  }
+);
+
 function App() {
+
+  useEffect(() => {
+    initAnalytics();
+  }, []);
+
+  //The default behavior across all <HotKeys> components
+  reactHotkeys.configure({
+    //Disable simulate keypress events for the keys that do not natively emit them
+    //When Enabled - events are not captured after using Enter in <textarea/>
+    simulateMissingKeyPressEvents: false,
+    //Clear the ignoreTags array to includes events on textarea and input
+    ignoreTags: [],
+  });
+
   return (
     <div className="App">
       <Provider store={store}>
@@ -28,4 +57,4 @@ function App() {
   );
 }
 
-export default App;
+export default enhance(App);
